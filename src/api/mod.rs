c8b35c97dac11f865ef70d6e07511a90b6371bb1
PRRPCHT <pierrepic@gmail.com>
2026-01-13 11:54:53 +0100

    Adding the access token response

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -24,3 +24,11 @@ pub struct PingResponse {
 pub struct EncryptionKeyResponse {
     pub encryption_key: String,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AccessTokenResponse {
+    pub access_token: String,
+    pub token_type: String,
+    pub expires_in: i64,
+    pub expires_at: String,
+}
